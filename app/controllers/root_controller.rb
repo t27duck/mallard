9b5f0fcb5763c0b3b53f06c5d25b56538e94884c
Tony Drake <t27duck@gmail.com>
2014-08-30 13:34:08 -0400

    Work on starred and unread pages

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,22 @@
 class Mallard < Sinatra::Base
   get "/" do
     @entries = Decorator.generate(EntryRepo.unread)
+    @page_title = I18n.translate("titles.unread_entries")
+    @active_menu = :unread_entires
+    erb :root
+  end
+
+  get "/starred" do
+    @entries = Decorator.generate(EntryRepo.starred)
+    @page_title = I18n.translate("titles.starred_entries")
+    @active_menu = :starred_entires
+    erb :root
+  end
+
+  get "/read" do
+    @entries = Decorator.generate(EntryRepo.read)
+    @page_title = I18n.translate("titles.read_entries")
+    @active_menu = :read_entires
     erb :root
   end
 end
