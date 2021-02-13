e36fe0844784341ea57daec1e9152a974cd41d6f
Tony Drake <t27duck@gmail.com>
2021-02-13 18:11:33 -0500

    Use webpacker 6 beta and tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './app/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/packs/**/*.js'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
