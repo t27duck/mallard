f205667da1173807a6653547405f823c929ff3f2
Tony Drake <t27duck@gmail.com>
2021-04-23 17:37:18 -0400

    Webpacker and JS packages updates

--- config/webpack/base.js
+++ config/webpack/base.js
@@ -1,18 +1,6 @@
 const { webpackConfig, merge } = require('@rails/webpacker')
 
-// MiniCssExtractPlugin is already loaded as a plugin, but in dev mode it does
-// not include the content hash. This removes the plugin, and we will make our
-// own instance of it to always include contentHash in the resulting file name.
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-webpackConfig.plugins = webpackConfig.plugins.filter(p => p.constructor.name != 'MiniCssExtractPlugin')
-
 const customConfig = {
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'css/[name]-[contenthash:8].css',
-      chunkFilename: 'css/[id]-[contenthash:8].css'
-    })
-  ],
   resolve: {
     extensions: ['.css', '.scss']
   }
