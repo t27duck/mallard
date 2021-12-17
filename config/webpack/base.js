7af7c5ff8981aa674df5b18fb315ba6b538f1cef
Tony Drake <t27duck@gmail.com>
2021-12-16 20:26:13 -0500

    Webpacker cleanup

--- config/webpack/base.js
+++
@@ -1,9 +0,0 @@
-const { webpackConfig, merge } = require('@rails/webpacker')
-
-const customConfig = {
-  resolve: {
-    extensions: ['.css', '.scss']
-  }
-}
-
-module.exports = merge(webpackConfig, customConfig)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,13 +13,6 @@ Capybara.server = :puma, { Silent: true }
 # Have tailwind jit do a one off build instead of keeping the process in memory to watch for file changes.
 ENV["TAILWIND_MODE"] ||= "build"
 
-# Possible workaround for "random" ActionView::Template::Error: 767: unexpected token at ''
-# This effectively will trigger webpacker on suite start and will result in building
-# assets first (if needed) before running any tests instead of having a separate
-# webpacker process per test worker which can step on each other.
-# https://github.com/rails/webpacker/issues/2860
-Webpacker.manifest.lookup("missing.js")
-
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
