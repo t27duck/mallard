a2e43901e961dbb9a41403be04c7924e35cbc5f2
Tony Drake <t27duck@gmail.com>
2024-01-06 10:28:29 -0500

    Vendor pined importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,9 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js"
+pin "el-transition" # @0.0.7
