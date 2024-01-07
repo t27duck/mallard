976a478f6dfd1076ceb333ebce3853c5ce199ee0
Tony Drake <t27duck@gmail.com>
2024-01-07 15:25:41 -0500

    Adjust UI styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,20 +13,20 @@ module ApplicationHelper
 
   def prev_link
     page = params[:page].to_i.zero? ? params[:page].to_i : (params[:page].to_i - 1)
-    link_to read_entries_path(page: page, search: params[:search]), class: "primary-color relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" do
+    link_to read_entries_path(page: page, search: params[:search]), class: "primary-color relative inline-flex items-center p-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" do
       "&laquo;".html_safe
     end
   end
 
   def number_of_pages
-    tag.span class: "relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" do
+    tag.span class: "relative inline-flex items-center p-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50" do
       "#{params[:page].to_i + 1} / #{@total_pages}"
     end
   end
 
   def next_link
     page = params[:page].to_i + 1 >= @total_pages ? params[:page].to_i : (params[:page].to_i + 1)
-    link_to read_entries_path(page: page, search: params[:search]), class: "primary-color relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" do
+    link_to read_entries_path(page: page, search: params[:search]), class: "primary-color relative inline-flex items-center p-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50" do
       "&raquo;".html_safe
     end
   end
