1a24f18fbc520d9c1d61eef2eb388d7dbc1f07f5
Tony Drake <t27duck@gmail.com>
2024-01-12 17:31:57 -0500

    Require plugin

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "../lib/puma/plugin/refresh_feeds"
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
