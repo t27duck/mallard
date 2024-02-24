7a50de69dd87468063e0e5aeaea5d0ee510e1f57
Tony Drake <t27duck@gmail.com>
2024-02-24 16:51:31 -0500

    Redesign flash messages

--- app/javascript/controllers/dismiss_controller.js
+++ app/javascript/controllers/dismiss_controller.js
@@ -3,8 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["content"]
 
+  connect() {
+    setTimeout(() => { this.close(); }, 3000)
+  }
+
   close(event) {
-    event.preventDefault()
+    if (event) {
+      event.preventDefault()
+    }
     this.contentTarget.addEventListener('transitionend', () => this.contentTarget.remove());
     this.contentTarget.style.opacity = '0'
   }
