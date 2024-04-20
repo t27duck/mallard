66a9d29ee575e785c1b2471776e6a971be25ccf4
Tony Drake <t27duck@gmail.com>
2024-04-20 10:02:06 -0400

    Try app with Rails edge

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ class Turbo::StreamsChannel; end unless defined?(ActionCable)
 module Mallard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
