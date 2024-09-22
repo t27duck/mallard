c71b83bd54cdeff818343d3a8b2cfcfd2920b89c
Tony Drake <t27duck@gmail.com>
2024-09-22 17:09:44 -0400

    Redo pwa code for Rails 7.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Render dynamic PWA files from app/views/pwa/*
+  get "service-worker" => "rails/pwa#service_worker", :as => :pwa_service_worker
+  get "serviceWorker" => "rails/pwa#service_worker", :as => :pwa_service_worker_alt
+  get "manifest" => "rails/pwa#manifest", :as => :pwa_manifest
+
   resource :setup, only: [:show, :create]
   resource :session, only: [:new, :create, :destroy]
   resources :feeds do
