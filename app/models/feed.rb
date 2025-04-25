e43d309c73b19e5b04fc7af57e357759596f0a79
Tony Drake <t27duck@gmail.com>
2025-04-24 20:47:11 -0400

    Remove entries via async

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,7 +4,7 @@ require "net/http"
 
 class Feed < ApplicationRecord
   ADDITIONAL_UNREAD_ENTRIES_TO_KEEP = 5
-  has_many :entries, dependent: :destroy
+  has_many :entries, dependent: :destroy_async
 
   validates :title, :url, presence: true
 
