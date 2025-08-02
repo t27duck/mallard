caebe30769c74b7b209ed8f20270a5a9fc0ffe2e
Tony Drake <t27duck@gmail.com>
2025-08-02 09:07:38 -0400

    Update asset ingegrity config

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+enable_integrity!
+
 # Pin npm packages by running ./bin/importmap
 
-pin "application", integrity: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", integrity: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", integrity: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", integrity: true
-pin_all_from "app/javascript/controllers", under: "controllers", integrity: true
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
